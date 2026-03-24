a1222def74ac89301971324415301a1694c18043
tomaschor <tomaschor@gmail.com>
2026-03-24 13:22:17 -0300

    Revert "squash merge tc/cpu-optimization: LocalCluster + HDF5 locking fix"
    This reverts commit 5db0b8ec6644f6aeb18180fea013a76565c14334.

--- submit_postprocessing_pbs.sh
+++ submit_postprocessing_pbs.sh
@@ -21,42 +21,22 @@ module li
 
 echo $CUDA_VISIBLE_DEVICES
 
-# Disable HDF5 file locking — Lustre (GPFS) advisory locks are unreliable and
-# cause EAGAIN failures when multiple dask worker processes write to the same file.
-export HDF5_USE_FILE_LOCKING=FALSE
-
 export JULIA_DEPOT_PATH="$WORK/.julia"
 export JULIA_CPU_TARGET="generic"
 juliaup default 1.12
 
-time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u \
-    postprocessing/01_filter_fields.py \
-    --filename output/khi_2916x1x4096.nc \
-    --n-workers 18 \
-    --threads-per-worker 3 \
-    2>&1 | tee logs/01_filter_fields_2916x1x4096.out
+time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u postprocessing/01_filter_fields.py --filename output/khi_2916x1x4096.nc 2>&1 | tee logs/01_filter_fields_2916x1x4096.out
 qstat -f $PBS_JOBID >> logs/01_filter_fields_2916x1x4096.log
 qstat -f $PBS_JOBID >> logs/01_filter_fields_2916x1x4096.out
 
-time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u \
-    postprocessing/02_energy_transfer.py \
-    --filename output/khi_2916x1x4096.nc \
-    --n-workers 18 \
-    2>&1 | tee logs/02_energy_transfer_2916x1x4096.out
+time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u postprocessing/02_energy_transfer.py --filename output/khi_2916x1x4096.nc --n-workers 18 2>&1 | tee logs/02_energy_transfer_2916x1x4096.out
 qstat -f $PBS_JOBID >> logs/02_energy_transfer_2916x1x4096.log
 qstat -f $PBS_JOBID >> logs/02_energy_transfer_2916x1x4096.out
 
-# time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u \
-#     postprocessing/03_sfs_ke_budget.py \
-#     --filename output/khi_2916x1x4096.nc \
-#     2>&1 | tee logs/03_sfs_ke_budget_2916x1x4096.out
+# time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u postprocessing/03_sfs_ke_budget.py --filename output/khi_2916x1x4096.nc 2>&1 | tee logs/03_sfs_ke_budget_2916x1x4096.out
 # qstat -f $PBS_JOBID >> logs/03_sfs_ke_budget_2916x1x4096.log
 # qstat -f $PBS_JOBID >> logs/03_sfs_ke_budget_2916x1x4096.out
 
-# time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u \
-#     postprocessing/04_sfs_ape_budget.py \
-#     --filename output/khi_2916x1x4096.nc \
-#     --n-workers 18 \
-#     2>&1 | tee logs/04_sfs_ape_budget_2916x1x4096.out
+# time /glade/u/home/tomasc/miniconda3/envs/py313/bin/python -u postprocessing/04_sfs_ape_budget.py --filename output/khi_2916x1x4096.nc --n-workers 18 2>&1 | tee logs/04_sfs_ape_budget_2916x1x4096.out
 # qstat -f $PBS_JOBID >> logs/04_sfs_ape_budget_2916x1x4096.log
 # qstat -f $PBS_JOBID >> logs/04_sfs_ape_budget_2916x1x4096.out
