cc543d4829ea0883452a9e0dcae7ba5b9e40f386
tomchor <tomaschor@gmail.com>
2026-03-30 04:37:35 -0600

    move submit_sweep script

--- postprocessing/submit_sweep_pbs.sh
+++ postprocessing/submit_sweep_pbs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 #PBS -A UMCP0028
-#PBS -N sweep_2048x1x2048
-#PBS -o logs/sweep_2048x1x2048.log
-#PBS -e logs/sweep_2048x1x2048.log
+#PBS -N sweep_1458x1x2048
+#PBS -o logs/sweep_1458x1x2048.log
+#PBS -e logs/sweep_1458x1x2048.log
 #PBS -l walltime=23:59:00
 #PBS -q casper
 #PBS -M tchor@umd.edu
 #PBS -m ae
 #PBS -r n
-#PBS -l select=1:ncpus=18:mem=1400GB:ngpus=0
+#PBS -l select=1:ncpus=18:mem=1000GB:ngpus=0
 #PBS -l job_priority=premium
 
-SIM=khi_2048x1x2048
+SIM=khi_1458x1x2048
 PYTHON=/glade/u/home/tomasc/miniconda3/envs/py313/bin/python
 
 # Clear the environment from any previously loaded modules
@@ -28,10 +28,10 @@ export JULIA_DEPOT_PATH="$WORK/.julia"
 export JULIA_CPU_TARGET="generic"
 juliaup default 1.12
 
-time $PYTHON -u postprocessing/inv1_filter_fields_sweep.py --filename output/${SIM}.nc 2>&1 | tee logs/inv1_filter_fields_sweep_${SIM}.out
+time $PYTHON -u inv1_filter_fields_sweep.py --filename output/${SIM}.nc 2>&1 | tee logs/inv1_filter_fields_sweep_${SIM}.out
 qstat -f $PBS_JOBID >> logs/inv1_filter_fields_sweep_${SIM}.log
 qstat -f $PBS_JOBID >> logs/inv1_filter_fields_sweep_${SIM}.out
 
-time $PYTHON -u postprocessing/inv2_energy_transfer_sweep.py --filename output/${SIM}.nc --n-workers 18 2>&1 | tee logs/inv2_energy_transfer_sweep_${SIM}.out
+time $PYTHON -u inv2_energy_transfer_sweep.py --filename output/${SIM}.nc --n-workers 18 2>&1 | tee logs/inv2_energy_transfer_sweep_${SIM}.out
 qstat -f $PBS_JOBID >> logs/inv2_energy_transfer_sweep_${SIM}.log
 qstat -f $PBS_JOBID >> logs/inv2_energy_transfer_sweep_${SIM}.out
