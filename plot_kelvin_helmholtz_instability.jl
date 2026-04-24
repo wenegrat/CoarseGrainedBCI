22df781944f2b0ac9ca5887fb14d13e96de40368
tomaschor <tomaschor@gmail.com>
2026-04-24 10:21:55 -0300

    plot filtered fields as well

--- plot_kelvin_helmholtz_instability.jl
+++ plot_kelvin_helmholtz_instability.jl
@@ -20,8 +20,9 @@ end
 #---
 
 #+++ Read simulation parameters from file global attributes
-Re, Ri, Pr = NCDataset(plot_filepath, "r") do ds
-    Float64(ds.attrib["Re"]), Float64(ds.attrib["Ri"]), Float64(ds.attrib["Pr"])
+Re, Ri, Pr, filter_widths = NCDataset(plot_filepath, "r") do ds
+    fw = Tuple(Float64(ds.attrib["filter_width_$i"]) for i in 1:3)
+    Float64(ds.attrib["Re"]), Float64(ds.attrib["Ri"]), Float64(ds.attrib["Pr"]), fw
 end
 #---
 
@@ -30,11 +31,16 @@ end
 ω_timeseries = FieldTimeSeries(plot_filepath, "ω", architecture=CPU())
 b_timeseries = FieldTimeSeries(plot_filepath, "b", architecture=CPU())
 S_timeseries = FieldTimeSeries(plot_filepath, "S", architecture=CPU())
+b_filt1_timeseries = FieldTimeSeries(plot_filepath, "b_filt1", architecture=CPU())
+b_filt2_timeseries = FieldTimeSeries(plot_filepath, "b_filt2", architecture=CPU())
+b_filt3_timeseries = FieldTimeSeries(plot_filepath, "b_filt3", architecture=CPU())
 
-# The architecture of the FieldTimeSeries isn't working as expected, so load on CPU manually:
 ω_timeseries = on_architecture(CPU(), ω_timeseries)
 b_timeseries = on_architecture(CPU(), b_timeseries)
 S_timeseries = on_architecture(CPU(), S_timeseries)
+b_filt1_timeseries = on_architecture(CPU(), b_filt1_timeseries)
+b_filt2_timeseries = on_architecture(CPU(), b_filt2_timeseries)
+b_filt3_timeseries = on_architecture(CPU(), b_filt3_timeseries)
 
 times = ω_timeseries.times
 #---
@@ -42,11 +48,14 @@ times = ω_timeseries.times
 #+++ Build figure
 n = Observable(1)
 
-ωₙ = @lift view(ω_timeseries[$n], :, 1, :)
-bₙ = @lift view(b_timeseries[$n], :, 1, :)
-Sₙ = @lift view(S_timeseries[$n], :, 1, :)
+ωₙ  = @lift view(ω_timeseries[$n], :, 1, :)
+bₙ  = @lift view(b_timeseries[$n], :, 1, :)
+Sₙ  = @lift view(S_timeseries[$n], :, 1, :)
+bf1ₙ = @lift view(b_filt1_timeseries[$n], :, 1, :)
+bf2ₙ = @lift view(b_filt2_timeseries[$n], :, 1, :)
+bf3ₙ = @lift view(b_filt3_timeseries[$n], :, 1, :)
 
-fig = Figure(size=(1200, 500))
+fig = Figure(size=(1200, 900))
 
 params_str = @sprintf("Re = %d,  Ri = %.2f,  Pr = %d", Re, Ri, Pr)
 title = @lift @sprintf("Kelvin-Helmholtz Instability  (%s)\nt = %.1f", params_str, times[$n])
@@ -66,6 +75,20 @@ Colorbar(fig[2, 4], hm_b)
 
 hm_S = heatmap!(ax_S, Sₙ; colormap=:thermal)
 Colorbar(fig[2, 6], hm_S)
+
+b_crange = (-0.08, 0.08)
+ax_bf1 = Axis(fig[3, 1]; title=@sprintf("Filtered b (Δ = %.1f)", filter_widths[1]), kwargs...)
+ax_bf2 = Axis(fig[3, 3]; title=@sprintf("Filtered b (Δ = %.1f)", filter_widths[2]), kwargs...)
+ax_bf3 = Axis(fig[3, 5]; title=@sprintf("Filtered b (Δ = %.1f)", filter_widths[3]), kwargs...)
+
+hm_bf1 = heatmap!(ax_bf1, bf1ₙ; colormap=:balance, colorrange=b_crange)
+Colorbar(fig[3, 2], hm_bf1)
+
+hm_bf2 = heatmap!(ax_bf2, bf2ₙ; colormap=:balance, colorrange=b_crange)
+Colorbar(fig[3, 4], hm_bf2)
+
+hm_bf3 = heatmap!(ax_bf3, bf3ₙ; colormap=:balance, colorrange=b_crange)
+Colorbar(fig[3, 6], hm_bf3)
 #---
 
 #+++ Record animation
