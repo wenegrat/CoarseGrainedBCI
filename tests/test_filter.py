09fadbf92dd96365d9728317ec90b2a0d4cacd9a
tomchor <tomaschor@gmail.com>
2026-05-06 06:59:02 -0600

    fix tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -10,7 +10,7 @@ import xarray as xr
 import pytest
 
 sys.path.insert(0, str(Path(__file__).parent.parent / "postprocessing"))
-from aux00_utils import GaussianFilter
+from src.aux00_utils import GaussianFilter
 
 
 def _measure_fwhm(values, coords):
